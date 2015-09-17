d536595d3fb24389a9ba386eb63063bf7ad75065
yutong pang <yutongpang@yutongs-iMac.home>
2015-09-17 10:37:28 -0400

    add test b for Inverse1D abstract class

--- src/test/scala-2.11/example/inverse1D/Inverse1DTEST.scala
+++ src/test/scala-2.11/example/inverse1D/Inverse1DTEST.scala
@@ -10,10 +10,10 @@ class Inverse1DTEST extends FunSuite {
   test("Inverse1DClass") {
     class Inverse1Dtest(matrixSize: Int) extends Inverse1D(matrixSize: Int){
       protected def initA(matrixSize: Int): DenseMatrix[Double] = DenseMatrix.zeros[Double](matrixSize, matrixSize)
-
       protected def initX(matrixSize: Int): DenseVector[Double] = DenseVector.zeros[Double](matrixSize)
     }
     assert(new Inverse1Dtest(5).a.size == 25)
     assert(new Inverse1Dtest(5).x.size == 5)
+    assert(new Inverse1Dtest(5).b.size == 5)
   }
 }
